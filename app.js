0e71f96e120e1dc13e34e5635a54ac10992a4d1a
kdncode <kdncode@gmail.com>
2018-04-22 15:34:44 -0500

    finished movie search api

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ var app = express();
 
 app.set("view engine", "ejs")
 
+app.get("/", (req, res) => {
+    res.render("search")
+})
+
 app.get("/results", (req, res) => {
-    request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", (error, response, body) => {
+
+    // Get  data from the query string
+    var myQuery = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + myQuery + "&apikey=thewdb";
+
+    // Get data
+    request(url, (error, response, body) => {
         if( !error && res.statusCode == 200 ) {
             var data = JSON.parse(body)
             res.render("results", {data: data})
